523424a5c1cc7698447a84b24a005777e6968f92
Joy Lin <vvlnote@hotmail.com>
2019-03-05 22:25:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,8 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, values|
     puts "#{key}, #{values}"
+    values.each do |detail_key, detail_value|
+      puts "detail_key = #{detail_key}, detail_value = #{detail_value}"
+    end
   end
 end
