eb793d6256abf89d6ba4fa1750ffede2fe0f1f4a
Joy Lin <vvlnote@hotmail.com>
2019-03-05 23:31:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each do |key, values|
-    puts "key = #{key}, values = #{values}"
+    #puts "key = #{key}, values = #{values}"
     values.each do |detail_key, detail_values|
       detail_values.each do |name|
-        
-        #puts "detail_key = #{detail_key}, detail_values = #{detail_values}"
         if new_hash.has_key? name
           if new_hash[name].has_key?(key)
             new_hash[name][key] << detail_key
@@ -21,7 +19,6 @@ def nyc_pigeon_organizer(data)
         end
       end
     end
-    puts "new_hash = #{new_hash}"
   end
   puts "new_hash = #{new_hash}"
   new_hash
