7eec57522022fda4683158d496dda1df6b2abf55
Joy Lin <vvlnote@hotmail.com>
2019-03-05 22:54:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, values|
     values.each do |detail_key, detail_values|
-  #    detail_values.each do |item|
+      detail_values.each do |item|
   #      if new_hash.has_key? item
   #        if new_hash[item].has_key?(key)
   #          new_hash[item][key] << detail.key
@@ -16,7 +16,7 @@ def nyc_pigeon_organizer(data)
   #        new_hash[item][key] = []
   #        new_hash[item][key] << detail.key
   #      end
-  #    end
+      end
     end
   end
   new_hash
